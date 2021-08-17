10b4744c228e9dc7b7ed4e94c7cb6263d579b3de
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-08-17 14:17:12 -0700

    add testimonials and tweak styling

--- pages/index.js
+++ pages/index.js
@@ -37,9 +37,6 @@ const Home = () => {
           </div>
         </section>
         <section className={style.upperHomeRight}>
-          {/* <div className={style.homeLogo}>
-            <img src="/assets/FactCheckedMP_white-transparentBG.png" alt="logo"/>
-          </div> */}
           <div className={style.homeMainText}>
             <h3 className="headers">What we do</h3>
             <p>
@@ -51,9 +48,10 @@ const Home = () => {
       <section className={style.lowerHome}>
         <h2 className="headers">Testimonials</h2>
         <div className={style.testimonials}>
-          <Testimonial image="/assets/greenlantern.jpg" text={'"this is just a test testimonial, the final will be replaced by whatever the actual testimonial is!"'} />
-          <Testimonial image="/assets/greenlantern.jpg" text={'"this is just a test testimonial, the final will be replaced by whatever the actual testimonial is!"'} />
-          <Testimonial image="/assets/greenlantern.jpg" text={'"this is just a test testimonial, the final will be replaced by whatever the actual testimonial is!"'} />
+          <Testimonial image="/assets/breakthecycle.jpg" text={'Joshua Smith of Break the Cycle: "Justin has taken my internet show from a cool to watch long form show to something that can be more digestible to people on the run, by creating short form samples that get people interested. He has a been a key to my growing success and I couldn\'t do the show without him. Let this man help you. He does amazing work"'} />
+          <Testimonial image="/assets/peddlingFiction.png" text={'Johnny Profita of Peddling Fiction: "My podcast wouldn\'t be what it is today without Justin\'s dedication and hard work. He\'s a modern-day renaissance man; handling everything from audio/video editing and social media promotion to fan page management and guest bookings so I can focus on creating content. He jumps at every opportunity to help with all the challenges that come with podcasting. He\'s the best thing to happen to a technologically illiterate such as myself."'} />
+          <Testimonial image="/assets/LionsOfLiberty.png" text={'Brian McWilliams of Lions of Liberty Podcast: "We\'ve been working with Justin for a while now and it’s been a fantastic experience that\'s been turnkey, taking the onus of social media content creation off our busy plates. Justin really gets to know the audience, and his content and clip selection matches what our fans want to hear. It\'s giving us a great opportunity to grow the fan base and maximize on what we’re putting out."'} />
+          <Testimonial image="/assets/LosLibertinos.jpg" text={'Carlos Abelar of Los Libertinos: "Professionalism and attention to detail comes standard with Justin\'s output but what I find most important and key is that the working relationship is honest and constructive. He makes you feel like he\'s a partner in helping execute your goals. I highly recommend him for your team.”'} />
         </div>
       </section>
     </div>
